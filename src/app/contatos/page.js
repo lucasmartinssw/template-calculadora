690590db9c1683ba1ca0005cd56d81c7dc7de7c1
Lucas <lucasrmartins40@gmail.com>
2025-06-26 20:01:14 -0300

    Criando pag inicial

---
+++ src/app/contatos/page.js
@@ -0,0 +1,8 @@
+export default function Contatos() {
+    return (
+        <div className="a-full b-screen flex item-center" >
+            <h1>Contatos</h1>
+        </div>
+    
+    )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div className="h-screen flex flex-col align-center items-center justify-center ">
+        <div className="flex flex-col align-center items-center justify-center h-screen max-w-3xl gap-9" >
+          <h1 className="text-4xl font-bold">Bem vindo à Calculadora RPV</h1>
+          <h2 className="text-xl font-bold">Projeto base Next.Js + Tailwind + shadcn + Integração Backend</h2>
+          <p className="leading-relaxed-text-lg text-center"> Este projeto foi criado para demonstrar uma calculadora de operações matemáticas integrando frontend moderno com backend externo, utilizando as melhores práticas do ecossistema React/Next.Js</p>
+          <Button>Ir para a calculadora</Button>
+        </div>
+      </div>
+    </>
+  );
+}
